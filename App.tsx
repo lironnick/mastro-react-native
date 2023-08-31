a08e4d81f3b8bf5034d3ab7763591a5738817045
Tiago Henrique <lironnick@gmail.com>
2023-08-31 17:53:52 -0300

    funcionando...

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Alert,
+} from 'react-native';
+
+import { styles } from './styles';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  function handleSignIn() {
+    if (email === 'lironnick@gmail.com' && password === '123') {
+      setIsValid(true);
+    } else {
+      Alert.alert('Email ou senha inválidos!');
+      setIsValid(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        onChangeText={setEmail}
+        testID="input-email"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={setPassword}
+        testID="input-password"
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleSignIn}
+        testID="button-entrar"
+      >
+        <Text style={styles.buttonTitle}>Entrar</Text>
+      </TouchableOpacity>
+
+      {isValid && (
+        <Text style={styles.message}>Acesso Liberado!</Text>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  input: {
+    height: 56,
+    width: '100%',
+    borderWidth: 2,
+    borderRadius: 5,
+    marginBottom: 16,
+    padding: 16,
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: 'black',
+    height: 56,
+    width: '100%',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTitle: {
+    color: 'white',
+    fontSize: 18,
+  },
+  message: {
+    marginTop: 32,
+    fontSize: 18,
+    color: 'green',
+  },
+});
